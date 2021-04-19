7934a3612ae075492b1e52677d05d3da38b24f08
David <42349979+segdavids@users.noreply.github.com>
2021-04-19 17:34:00 +0100

    Add project files.

---
+++ Verint-Chat_API-Capture/Text_Capture_Service.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verint_Chat_API_Capture
+{
+    public partial class Text_Capture_Service : ServiceBase
+    {
+        public Text_Capture_Service()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
