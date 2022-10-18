6a54b285aac6bd9469afd7e1b9b3d5de4dfe3761
David A.O <42349979+segdavids@users.noreply.github.com>
2022-10-18 11:54:48 +1100

    more

---
+++ ClrTemp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClrTemp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DELETE ANY EXISTING FILE IN THE FOLDER
+            System.IO.DirectoryInfo di = new DirectoryInfo(@"C:\inetpub\wwwroot\Temp\");
+
+            foreach (FileInfo file in di.EnumerateFiles())
+            {
+                file.Delete();
+            }
+        }
+    }
+}
